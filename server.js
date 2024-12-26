280a8bbc10b68c8c03a7d74a1838256322b9f0b4
Dishant Nalwaya <dishantd442442@gmail.com>
2024-12-26 10:26:25 +0530

    feat: added Routes in main file

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require("./routes/authRoutes")
+
+
 
 const app = express();
 const PORT = 7001;
+
+
+//routes
+app.use("/api/",authRoutes);
 
 
 app.listen(()=>console.log('Server Running on Port',PORT))
