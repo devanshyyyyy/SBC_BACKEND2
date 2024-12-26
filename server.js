ca33d66979b8366183a640ed68c40498830d61d3
Dishant Nalwaya <dishantd442442@gmail.com>
2024-12-26 10:21:38 +0530

    Initialized Project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 7001;
+
+
+app.listen(()=>console.log('Server Running on Port',PORT))
+
+
+
