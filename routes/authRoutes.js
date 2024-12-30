e3255aeb3a9c049000c1eae9e2571afb96eb47f2
Devanshi_Mufti <devanshimufti@gmail.com>
2024-12-30 16:23:07 +0530

    Added the content of authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const { validateRequestBody } = require('../middleware/authMiddleware');  // Importing validateRequestBody middleware
+const {SignUp,SignIn} = require("../controllers/userController");
 
+const app = express.Router();
 
+//User
+app.post('/signup', validateRequestBody(['username', 'email', 'password']), SignUp);
+app.post('/signin', validateRequestBody(['email', 'password']), SignIn);
 
-module.exports = router;
+module.exports = app;
